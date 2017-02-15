79510afb0954de252a1c4019373b764cacde685a
sueanyv <valenciasue@hotmail.com>
2017-02-15 05:24:54 -0800

    added stuff

--- test/test.js
+++ test/test.js
@@ -3,21 +3,20 @@
 const greet = require('../lib/greet.js');
 const expect = require('chai').expect;
 
-describe('Greet module', function(){
-  describe('#sayHi', function(){
-  it('should return hey sugey' function(){
-    var result = greet.sayHey('sugey');
-    expect(greet).to.have.property('sayHey');
-    expect(result).to.equal('hey sugey!');
-  };
-  it ('should throw a missing name error',  function(){
-var result =greet.sayHey;
-expect(result).to.throw(Error);
-  });
-});
-describe('#sayBye', function(){
-  it('should return see ya later', function(){
-expect(result).to.equal('se ya later');
-});
-});
-});
+describe('Greet module', function() {
+      describe('#sayHi', function() {
+          it('should return hey sugey'function() {
+              var result = greet.sayHey('sugey');
+              expect(greet).to.have.property('sayHey');
+              expect(result).to.equal('hey sugey!');
+
+            }; it('should throw a missing name error', function() {
+              var result = greet.sayHey;
+              expect(result).to.throw(Error);
+            });
+          }); describe('#sayBye', function() {
+          it('should return see ya later', function() {
+            expect(result).to.equal('se ya later');
+          });
+        });
+      });
