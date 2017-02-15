1856f44ec59c1bed8e23f62de40229e4a4b11270
sueanyv <valenciasue@hotmail.com>
2017-02-15 04:21:43 -0800

    changes to files

--- test/test.js
+++ test/test.js
@@ -1,24 +1,23 @@
 'use strict',
 
-const greet = require('..lib/greet.js')
-const assert = require('assert')
+const greet = require('../lib/greet.js');
+const expect = require('chai').expect;
 
 describe('Greet module', function(){
   describe('#sayHi', function(){
   it('should return hey sugey' function(){
-    var result = greet.sayHi('sugey');
-    assert.ok(result === 'hey sugey', 'not equal to hey sugey' );
-  });
+    var result = greet.sayHey('sugey');
+    expect(greet).to.have.property('sayHey');
+    expect(result).to.equal('hey sugey!');
+  };
   it ('should throw a missing name error',  function(){
-    asser.throws(function(){
-      greet.sayHi();
-    }, 'error not thrown');
+var result =greet.sayHey;
+expect(result).to.throw(Error);
   });
 });
 describe('#sayBye', function(){
-  it('should return hey sugey', function(){
-    var bye = 'Good bye! Have a good day',
-assert.ok(bye === 'hey sugey'. 'not equal to hey sugey');
-  });
+  it('should return see ya later', function(){
+expect(result).to.equal('se ya later');
+});
 });
 });
